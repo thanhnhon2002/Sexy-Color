d0a50b06a9fd83834ee13fe4be909c342759864c
ReturnThis <nhon91ddt@gmail.com>
2024-05-23 14:12:22 +0700

    some fix

--- Assets/01.Game/02.Scripts/0.DarkcupGame/Ads/Admob/Scripts/AdmobAdIntertistial.cs
+++ Assets/01.Game/02.Scripts/0.DarkcupGame/Ads/Admob/Scripts/AdmobAdIntertistial.cs
@@ -20,7 +20,7 @@ public class AdmobAdInterstitial : AdmobAds
     }
     public override void Init()
     {
-        //move to load ads
+        //LoadAds();
     }
 
     public override bool IsAdsAvailable()
